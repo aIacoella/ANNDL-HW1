b5935ce584d6e494fab35d7349ce38b1aec2b6af
aiacoella <angeloiacoella3@gmail.com>
2023-11-08 12:05:55 +0100

    first comit

---
+++ SubmissionTester.py
@@ -0,0 +1,23 @@
+import SubmissionExample.model as submission_model
+from tensorflow.keras.applications.mobilenet import preprocess_input
+from tensorflow.keras import layers as tfkl
+from tensorflow import keras as tfk
+import numpy as np
+
+seed = 42
+np.random.seed(seed)
+
+data_file = np.load('./public_data.npz', allow_pickle=True)
+
+data = data_file["data"]
+labels = data_file["labels"]
+
+batch = data[0:10]
+
+model = submission_model.model("./SubmissionExample")
+
+preds = model.predict(batch)
+print(preds)
+
+# Expected output:
+print("Expected output:" + labels[0:10])
