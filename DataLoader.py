3eb2407305db65a2983826f4ca6b0e17c6aba0cb
aiacoella <angeloiacoella3@gmail.com>
2023-11-12 22:20:07 +0100

    Dataset cleaning integration

--- DataLoader.py
+++ DataLoader.py
@@ -1,5 +1,6 @@
 from sklearn.model_selection import train_test_split
 from tensorflow import keras as tfk
+import matplotlib.pyplot as plt
 import numpy as np
 seed = 42
 
@@ -12,6 +13,31 @@ def load_data(seed=42, path="./public_data.npz", test_size=0.2, val_size=0.2):
     y[y == "healthy"] = 0
     y[y == "unhealthy"] = 1
     y = y.astype(np.float32)
+
+    #Trolls
+    troll = data[338]
+    shrek = data[58]
+
+    fig, axes = plt.subplots(1, 2, figsize=(6, 3))
+    axes[0].imshow(troll / 255)
+    axes[1].imshow(shrek / 255)
+    plt.tight_layout()
+    plt.show()
+
+    print("Removing trolls and shreks")
+    initialDataN = data.shape[0]
+
+    mask = []
+    for i in range(data.shape[0]):
+        if np.array_equal(data[i], troll) or np.array_equal(data[i], shrek):
+            mask.append(False)
+        else:
+            mask.append(True)
+
+    data = data[mask]
+    y = y[mask]
+    print("Removed Images: " + str(initialDataN - data.shape[0]))
+
     X_train_val, X_test, y_train_val, y_test = train_test_split(
         data, y, random_state=seed, test_size=test_size, stratify=y)
     X_train, X_val, y_train, y_val = train_test_split(
